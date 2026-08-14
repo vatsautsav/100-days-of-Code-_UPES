2f9c8b94d3966b8524c300e2bd8eca4682a8372b
vatsautsav <vatsautsav7@gmail.com>
2026-08-14 09:53:04 +0530

    Add files via upload

---
+++ sum1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int a ,b ;
+    printf("Enter a ", a);
+    scanf("%d", &a);
+    printf("Enter b ", b);
+    scanf("%d", &b);
+
+    printf("Sum is : %d ", a + b);
+    return 0;   
+}
