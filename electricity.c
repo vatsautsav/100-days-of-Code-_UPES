e9adc67a5e970323a64ab87d41bd2dfb6f3d41b3
Utsav Vatsa <vatsautsav7@gmail.com>
2026-08-26 20:31:50 +0530

    Add files via upload

---
+++ electricity.c
@@ -0,0 +1,34 @@
+/* Write a program to calculate electricity bill based on units consumed with these rates: 
+First 100 units at ₹5/unit 
+Next 100 units at ₹7/unit 
+Next 100 units at ₹10/unit 
+Above at ₹12/unit */
+
+
+
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill;
+
+    printf("Enter electricity units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 100) {
+        bill = units * 5;
+    } 
+    else if (units <= 200) {
+        bill = (100 * 5) + (units - 100) * 7;
+    } 
+    else if (units <= 300) {
+        bill = (100 * 5) + (100 * 7) + (units - 200) * 10;
+    } 
+    else {
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + (units - 300) * 12;
+    }
+
+    printf("Electricity Bill = Rs%.2f\n", bill);
+
+    return 0;
+}
