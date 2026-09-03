7102901cf43ccacac871bbefb00d7ac9c4a88dcd
Utsav Vatsa <vatsautsav7@gmail.com>
2026-09-03 18:37:40 +0530

    Enhance Armstrong number program with digit count
    Refactor Armstrong number check to include digit count and use power function.

--- armstrong.c
+++ armstrong.c
@@ -2,28 +2,44 @@
 
 
 #include <stdio.h>
+#include <math.h>
 
-int main()
-{
-    int n, original, digit, sum = 0;
+int main()  {
+    int n, original, digits = 0, digit, sum = 0;
 
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    if (n < 0)  {
+    if (n < 0)   {
         printf("Please enter a non-negative number.\n");
-    }  else {
-        original = n;
+        return 0;
+    }
+    original = n;
 
-        while (n > 0)  {
-            digit = n % 10;
-            sum = sum + digit * digit * digit;
+    // Count the number of digits
+    if (n == 0)
+        digits = 1;
+        
+    else  {
+        while (n > 0)   {
+            digits++;
             n = n / 10;
         }
-        if (sum == original)
-            printf("%d is an Armstrong number.\n", original);
-        else
-            printf("%d is not an Armstrong number.\n", original);
     }
+    n = original;
+
+    // Calculate the Armstrong sum
+    while (n > 0)
+    {
+        digit = n % 10;
+        sum = sum + pow(digit, digits);
+        n = n / 10;
+    }
+    
+    if (sum == original)
+        printf("%d is an Armstrong number.\n", original);
+    else
+        printf("%d is not an Armstrong number.\n", original);
+
     return 0;
 }
