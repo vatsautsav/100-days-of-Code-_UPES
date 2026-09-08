344e23136212e65050a85b265048cb78878f3f11
Utsav Vatsa <vatsautsav7@gmail.com>
2026-09-08 21:57:09 +0530

    Add files via upload

---
+++ pattern_5.c
@@ -0,0 +1,34 @@
+/* Write a program to print the following pattern:
+
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 5; i++) {
+
+        // Print spaces
+        for (j = 1; j <= 5 - i; j++) {
+            printf(" ");
+        }
+
+        // Print numbers
+
+        for (j = 6 - i; j <= 5; j++) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
