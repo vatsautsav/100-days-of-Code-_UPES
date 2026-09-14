26952be0fbd3a249ba518bddd64c23c9c581d7b1
Utsav Vatsa <vatsautsav7@gmail.com>
2026-09-14 22:54:26 +0530

    Add files via upload

---
+++ reverse_array.c
@@ -0,0 +1,40 @@
+// Reverse an array without taking extra space.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+1 2 3 4
+Output 1:
+4 3 2 1
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n, arr[100], i, temp;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
