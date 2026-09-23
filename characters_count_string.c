0e4e047cbf9b1a56c2ef2259325afe0d41a2161c
Utsav Vatsa <vatsautsav7@gmail.com>
2026-09-23 23:40:18 +0530

    Add files via upload

---
+++ characters_count_string.c
@@ -0,0 +1,34 @@
+// Count characters in a string without using built-in length functions.
+
+/*
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+5
+
+Input 2:
+ 
+Output 2:
+1
+
+*/
+
+
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    while (str[count] != '\0' && str[count] != '\n') {
+        count++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
