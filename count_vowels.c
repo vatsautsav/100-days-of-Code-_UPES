6814ee53db9c89ead89604e7c20214ddd96e5776
Utsav Vatsa <vatsautsav7@gmail.com>
2026-09-24 23:13:10 +0530

    Add files via upload

---
+++ count_vowels.c
@@ -0,0 +1,37 @@
+// Count vowels and consonants in a string.
+
+/*
+Sample Test Cases:
+Input 1:
+hello
+Output 1:
+Vowels=2, Consonants=3
+
+*/
+
+
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, vowels = 0, consonants = 0;
+
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+            str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+            str[i] == 'O' || str[i] == 'U') {
+            vowels++;
+        }
+        else {
+            consonants++;
+        }
+    }
+
+    printf("Vowels=%d, Consonants=%d", vowels, consonants);
+
+    return 0;
+}
